557c2bf16c706616d8828d7d6ce66ce79f55845f
TartiBOULE2Neige <163644511+TartiBOULE2Neige@users.noreply.github.com>
2024-03-16 13:25:13 +0100

    Add files via upload

---
+++ files/php/instances.php
@@ -0,0 +1,20 @@
+<?php
+$instance['test01'] = array_merge($instance['test01'], array(
+    "loadder" => array(
+        "minecraft_version" => "1.12.2",
+        "loadder_type" => "forge",
+        "loadder_version" => "latest"
+    ),
+    "verify" => true,
+    "ignored" => array(
+        'config',
+        'options.txt',
+    ),
+    "whitelist" => array(),
+    "whitelistActive" => false,
+    "status" => array(
+        "nameServer" => "Vanadium",
+        "ip" => "TartiBOULE2Nei.aternos.me",
+        "port" => 25565
+    )
+));
